8f8a7440ce581bd125370b82161f0f0159200a68
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-28 00:20:56 +0530

    fuctions

---
+++ Functions/1.cpp
@@ -0,0 +1,16 @@
+// in a code if we want to print a thing agai  and again then we use function 
+
+#include<iostream>
+using namespace std;
+
+//function defination 
+int printHello(){
+    cout<<"hello\n";
+    return 3;
+}
+
+int main(){
+    //function call
+    printHello();
+
+}
