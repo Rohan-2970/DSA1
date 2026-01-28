0275fced67a5fc312a26ae91ab144c4f1b0660dd
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-29 01:42:40 +0530

    functions

---
+++ Functions/3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+// Type 3: No Arguments, Return Value
+// int getnumber(){
+//     return 10;
+// }
+
+// Type 4: Arguments + Return (MOST IMPORTANT)
+int square(int n){
+    return n*n;
+}
+
+int main(){
+    // cout<<getnumber();
+    int a;
+    cout<<"Enter the value sof a: ";
+    cin>>a;
+    cout<<square(a);
+
+}
+
+// 5️⃣ Function Call Stack (INTERVIEW FAVORITE)
+// Example:
+// main() → fun1() → fun2()
+
+
+// 📌 Functions execute in Last In First Out (LIFO) order
+// 📌 Stack memory is used
