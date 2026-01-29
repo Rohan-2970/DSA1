19a4ac25e9b93751c80725a172d3ca259bb566a4
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-30 00:10:55 +0530

    sum of 2 numbers

---
+++ Babbar/Functions/sum_of_2_no..cpp
@@ -0,0 +1,12 @@
+ #include<iostream>
+ using namespace std;
+
+ int sum(int a , int b){
+    int s=a+b;
+    return s;
+ }
+ int main(){
+    cout<<sum(54,65)<<endl;
+
+ }
+//  redundancy -- unnececary repetion of code 
