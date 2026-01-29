72d4d196cb7d445d1633b7581d3a983c2bff2079
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-30 00:33:49 +0530

    1

--- Babbar/Functions/sum_of_2_no..cpp
+++ Babbar/Functions/sum_of_2_no..cpp
@@ -1,12 +1,30 @@
  #include<iostream>
  using namespace std;
 
- int sum(int a , int b){
-    int s=a+b;
-    return s;
- }
- int main(){
-    cout<<sum(54,65)<<endl;
+// sum of 2 numbers
 
- }
+//  int sum(int a , int b){
+//     int s=a+b;
+//     return s;
+//  }
+
+// min of 2 numbers
+void min(float y, float x){
+   if(y>x){
+      cout<< "x is a greater number";
+   }
+   else if(y==x){
+      cout<<"they both are equal";
+   }
+   else
+   {
+      cout<<"y is greater than x";
+   }
+   
+}
+
+int main(){
+   min(54, 65);
+
+}
 //  redundancy -- unnececary repetion of code 
