f2e14b79d41109c4b6c15529bd63242cb91f2d53
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-01 02:03:29 +0530

    loop

--- Array/1.cpp
+++ Array/1.cpp
@@ -11,8 +11,8 @@
 using namespace std;
 int main(){
     int marks[100] = {99,45,65,54};
-    double price[] ={98.99 , 105.34,243.56};
-    cout<<marks[0];
-    cout<<marks[1]<<endl;
-    cout<<marks[2]<<endl;
-}
+//     // double price[] ={98.99 , 105.34,243.56};
+//     // cout<<marks[0]<<endl;
+//     // cout<<marks[1]<<endl;
+//     // cout<<marks[2]<<endl;
+// }
---
+++ Array/Loop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[6] = {67,87,90,136,80};
+
+    
+    //loop: 0- size-1
+
+
+}
