22bf62bedd15a9b538e71802801f1deaaa80991b
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-01 01:55:03 +0530

    arrays

---
+++ Array/1.cpp
@@ -0,0 +1,18 @@
+// DS - structure to stotre data
+// 
+// why we learn dsa to learn code to build real life systems
+
+// Array - 
+// insta me har client ka ek alag var thodi banaunga there array
+// same type of data in a array
+// contigous in memory
+
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[100] = {99,45,65,54};
+    double price[] ={98.99 , 105.34,243.56};
+    cout<<marks[0];
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+}
