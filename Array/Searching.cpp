2ff228ec245d080f5823eb5411cb72c5b4aded5b
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-08 15:41:02 +0530

    use of vector

--- Array/Searching.cpp
+++ Array/Searching.cpp
@@ -2,6 +2,7 @@
 
 // Linear Search (Unsorted Array)
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
     int n , key;
@@ -9,7 +10,7 @@ int main(){
     cout<<"Enter number of elements: ";
     cin>>n;
 
-    int arr[n];
+    vector<int> arr(n);
     cout<<"Enter the "<<n<<" number of elements: \n";
     for(int i=0;i<n; i++){
         cin>>arr[i];
