80f52d62ff626aa0dac0ff798cd8da0812538282
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-12 01:30:06 +0530

    binary_search

--- Chatgpt practice/Day-4/Binary.cpp
+++ Chatgpt practice/Day-4/Binary.cpp
@@ -6,26 +6,75 @@
 
 // if not sorted → Binary Search will fail
 
-// Array:  [10, 20, 30, 40, 50]
-// Target: 30
+// Array:  [10, 20, 30, 40, 50]     
+// Target: 40
 // Steps:
+// 40 greater than 30 so grater than  30 side search now 
+// now there mid element mid = (3+4)/2 = 7/2 = 3
+// 40 == 40 --> true
 
 // Find the middle
 
 // Compare middle with target
 // If equal → done
 // If target is smaller → go left
-// If target is larger → go right
+// If target is larger → go right then take the mid 
+// elemet divide it by 2 and then compare it 
 
 // elements shloud be in monotonic fuction 
 // Repeat until found or range becomes invalid
 
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int binarySearch(int arr[], int n, int key){
-    int low = 0;
-    int high = n-1;
+    int start = 0;
+    int end = n-1;
+
+    int mid = (start + end)/2;
+
+    while(start<=end){
+       if(arr[mid] == key){
+        return mid;
+       }   
+       if(arr[mid]<key){
+        start = mid+1;
+       }
+       else{
+        end = mid-1;
+       }
+       mid = (start + end)/2;
+    }
+    return -1;
+}
+
+int main(){
+    // int even[5]= {3,4,5,6,8};
+    // int odd[6] = {2,4,5,6,18};
+    // int evenindex = binarySearch(even, 6, 18);
+    // cout<<"Index of 18 is "<<evenindex <<endl;
+
+    int n,key;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    vector<int> arr(n);
+    cout<<"Enter the values of "<<n<<": ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+cout<<"Enter the element to search: ";
+cin>>key;
+
+int result = binarySearch(arr.data(), n ,key);
+if(result != -1){
+    cout<<"Element found at index: "<<result<<endl;
+}
+else{
+    cout<<"Elements not found"<<endl;
+}
+
 
 }
