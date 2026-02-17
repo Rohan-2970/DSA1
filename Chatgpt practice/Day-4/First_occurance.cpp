e5935ac79331d526799d8bd2acaee3df956255f4
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-17 10:28:35 +0530

    firct occurance

---
+++ Chatgpt practice/Day-4/First_occurance.cpp
@@ -0,0 +1,14 @@
+// When duplicates exist in a sorted array, normal 
+// binary search:
+
+// Stops at the first match it finds
+// That match may NOT be the first or last occurrence
+
+// arr = [2,3,4,5,5,5,7,11]
+
+// In case of first occurance 
+// first = 3 , last = 3
+// here when we go to the next position then 
+// first = 4 . last = 3
+// again in the next step 
+// first = 5 , last = 3
