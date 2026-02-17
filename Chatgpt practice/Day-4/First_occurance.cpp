485e947bbfe3705939b80359d0150e5a8553a2ba
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-17 18:22:20 +0530

    code_first_occ

--- Chatgpt practice/Day-4/First_occurance.cpp
+++ Chatgpt practice/Day-4/First_occurance.cpp
@@ -8,6 +8,8 @@
 
 // initaily - we take first = -1 , last = -1
 
+// this is the case for the linear search 
+
 // In case of first occurance 
 // first = 3 , last = 3
 // here when we go to the next position then 
@@ -20,4 +22,38 @@
 
 // lower bound  = mid > n
 // upper bound  = mid <n
+
+// 2log(n) - when use both the upper and lower bound
+
+// arr = [2,3,5, 5 ,5,5,7,11]
+//       low    mid     high
+// binary search  
+
+// first occurance - the mid to the left 
+// last occurance - the mid to right 
+
+
+#include<iostream>
+using namespace std;
+
+int firstOccurence(int arr[], int n, int key){
+    int low = 0, high = n-1;
+    int first = -1;
+
+    while(low<=high){
+        int mid = low + (high - low)/2;
+        if(arr[mid]==key){
+            first = mid;
+            high = mid-1;
+        }
+        else if(arr[mid]<key){
+            
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return first;
+}
 
