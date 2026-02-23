62bbcb3ef07fe77a7faa3e29b7da156dc36b4dd6
Rohan-2970 <rohansaxena0905@gmail.com>
2026-02-23 23:11:14 +0530

    practice

--- practice/binary.cpp
+++ practice/binary.cpp
@@ -2,39 +2,42 @@
 #include<vector>
 using namespace std;
 
-int binarysearchalgo(int arr[],int n, int key){
+int binarySearchalgo(int arr[], int n, int key){
     int start = 0;
     int end = n-1;
-    int mid = (start + end)/2;
-    if(arr[mid] == key){
-        return mid;
-    }
-    if(arr[mid]<key){
-        return mid+1;
-    }
-    else if(arr[mid]>key){
-        return mid-1;
+
+    int mid = start + (end-start)/2;
+    while (start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(arr[mid]<key){
+            start = mid + 1;
+        }
+        if(arr[mid]>key){
+            end = mid-1;
+        }
+
     }
     return -1;
 }
+
 int main(){
-    int n,key;
-    cout<<"Enter a number: ";
+    int n, key;
+    cout<<"Enter the elements: ";
     cin>>n;
+
     vector<int> arr(n);
-    cout<<"Enter the "<<n<<" element values: ";
-    for(int i = 0; i<n; i++){
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"Enter the key: ";
+    cout<<"Enter the element: ";
     cin>>key;
-
-    int result = binarysearchalgo(arr.data(),n,key);
-    if(result != -1){
-        cout<<"Element foud at the index "<<result<<endl;
+    int r = binarySearchalgo(arr.data(),n,key);
+    if(r != -1){
+        cout<<"Element faound at index: "<<r<<endl;
     }
     else{
-        cout<<"Element is not found at the arrays ";
+        cout<<"Element not found: "<<endl;
     }
-
 }
