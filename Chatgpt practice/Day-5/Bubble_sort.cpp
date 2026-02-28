1bf0b75e9b263571e89863273d684c11c0700732
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-01 01:55:59 +0530

    main_code

--- Chatgpt practice/Day-5/Bubble_sort.cpp
+++ Chatgpt practice/Day-5/Bubble_sort.cpp
@@ -45,5 +45,30 @@
 
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the elements: ";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter each element: ";
+    for(int i = 0; i<n;i++){
+        cin>>arr[i];
+    }
+
+    // bubble sort
+    for(int i = 0; i<n-1;i++){
+        for(int j = 0; j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 
