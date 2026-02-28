08159dec242b6b64f2889749018685985445edc2
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-01 00:38:10 +0530

    bubble sort

---
+++ Chatgpt practice/Day-5/Bubble_sort.cpp
@@ -0,0 +1,22 @@
+// Sorting  
+// Sorting means arranging data in specific order to make it easy to use 
+// 1. Ascending order - (1,2,3,4,5)
+// 2. Descending order - (9,3,2,1)
+
+// BUBBLE SORT
+
+// It sorts by comparing adjacent elements 
+// Swapping them if they are in the wrong order 
+// After each full pass -> largest element moves to end 
+
+// 5 4 3 1 
+// Pass 1 - compare 5 and 1 -> swap 
+// 4 5 3 1
+// compare 5 and 3 -> swap
+// 4 3 5 1
+// compare 5 and 1 -> swap
+// 4 3 1 5
+
+#include<iostream>
+using namespace std;
+
