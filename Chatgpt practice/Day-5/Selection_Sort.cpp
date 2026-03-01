41d81448622029a500c198201d83048b26498d77
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-02 00:46:15 +0530

    Selection_sort

--- Chatgpt practice/Day-5/Selection_Sort.cpp
+++ Chatgpt practice/Day-5/Selection_Sort.cpp
@@ -1,8 +1,8 @@
 // Selection Sort
 
-// We find the smallest element 
-// Swap the smallest element in the arr to the
-// element in the index 0 
+// We find the minimum element 
+// Swap the minimum element in the arr to the
+// element in the index 0 and then it will go to min elemt place 
 // Then expand the sorted position one by one 
 
 // 5 1 4 2
@@ -32,3 +32,36 @@
 // Result:
 
 // 1 2 4 5
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the elements: ";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter the value of each element: ";
+    for(int i = 0; i<n-1; i++){
+        cin>>arr[i];
+    }
+
+    //Selection Sort
+    for(int i = 0; i<n-1; i++){
+        int MinIndex = i;
+
+        //Find smallest element in unsorted part
+        for(int j = i+1; j<n; j++){
+            if(arr[j]<arr[MinIndex]){
+                MinIndex = j;
+            }
+        }
+            // Swap smallest element with curretn position
+            swap(arr[i], arr[MinIndex]);
+    }
+    cout<<"Sorted array: ";
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
