2309660bee6e6c7b3ca10dcc7895cb7d83d69c79
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-01 13:27:42 +0530

    Optimize_bubble sort

--- Chatgpt practice/Day-5/optimized_code.cpp
+++ Chatgpt practice/Day-5/optimized_code.cpp
@@ -12,14 +12,32 @@ int main(){
         cin>>arr[i];
     }
     // bubble sort 
+
+    // understanding --
+
+// we used bool swapped to check whether any 
+// swapping happened in current phase if the
+//  array is already sorted, in normal 
+// "for loop" of bubble sort it checks the 
+// element again and again even if the array 
+// is already sorted ex-- 1 2 3 4 5 it will 
+// check again and again if it is sorted or not 
+
+
+    bool swapped;
     for(int i = 0; i<n-1; i++){
+        swapped = false;
+        
         for(int j = 0; j<n-i-1; j++){
-            if(arr[j]>arr[j+1]){ // yaha pe j > j+1 
-// aye toh humlog ko swap karna hai no.s ---- matlab 
-// 1 2 3 5 4 -- j= 5 and j+1 = 4 toh bada no. ko last 
-// me ane ke liye swap karna hai
+            if(arr[j]>arr[j+1]){
                 swap(arr[j] , arr[j+1]);
+                swapped = true;
             }
+        }
+// if no swap happens the entire timw thwn 
+// this if runs and loop stops 
+        if(!swapped){
+            break;
         }
     }
     cout<<"Sorted array: ";
