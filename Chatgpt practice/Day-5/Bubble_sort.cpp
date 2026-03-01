9785e77e052c054f54a38ca81166b9f183a6abaf
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-01 11:55:20 +0530

    c

--- Chatgpt practice/Day-5/Bubble_sort.cpp
+++ Chatgpt practice/Day-5/Bubble_sort.cpp
@@ -61,7 +61,10 @@ int main(){
     // bubble sort
     for(int i = 0; i<n-1;i++){
         for(int j = 0; j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]>arr[j+1]){// yaha pe j > j+1 
+// aye toh humlog ko swap karna hai no.s ---- matlab 
+// 1 2 3 5 4 -- j= 5 and j+1 = 4 toh bada no. ko last 
+// me ane ke liye swap karna hai
                 swap(arr[j],arr[j+1]);
             }
         }
