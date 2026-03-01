b5d16a9cb6e929da78d7a72ba40ee9b9b32ab233
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-01 14:52:14 +0530

    Selection Sort

---
+++ Chatgpt practice/Day-5/Selection_Sort.cpp
@@ -0,0 +1,34 @@
+// Selection Sort
+
+// We find the smallest element 
+// Swap the smallest element in the arr to the
+// element in the index 0 
+// Then expand the sorted position one by one 
+
+// 5 1 4 2
+// ➤ Pass 1 (i = 0)
+// Find smallest from index 0 to 3.
+
+// Smallest = 1
+// Swap with arr[0].
+// Result:
+
+// 1 5 4 2
+
+// ➤ Pass 2 (i = 1)
+// Find smallest from index 1 to 3.
+// Smallest = 2
+// Swap with arr[1].
+
+// Result:
+
+// 1 2 4 5
+
+// ➤ Pass 3 (i = 2)
+
+// Find smallest from index 2 to 3.
+// Smallest = 4
+// Swap with arr[2].
+// Result:
+
+// 1 2 4 5
