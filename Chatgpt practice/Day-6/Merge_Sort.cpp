63253c21d5631d75b2e09ef3d602129863bc2f7d
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-08 14:44:29 +0530

    Merge_sort

--- Chatgpt practice/Day-6/Merge_Sort.cpp
+++ Chatgpt practice/Day-6/Merge_Sort.cpp
@@ -43,4 +43,70 @@
 // first divide the left part 
 
 // Psudocode:
-// 
+
+// 3 2 4 1 3
+//low - starting no.   high - last element 
+// mergeSort(arr, low, high){
+// mid = (low + high)/2
+// mergeSort()
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(int arr[], int l, int mid, int r){
+    int n1 = mid-l+1;
+    int n2 = r-mid;
+    vector<int>left(n1);
+    vector<int>right(n2);
+
+    for(int i=0; i<n1;i++){
+        left[i] = arr[l+i];
+    }
+    for(int j = 0; j<n2; j++){
+        right[j] = arr[mid+1+j];
+    }
+    int i=0,j=0,k=l;
+
+    while(i<n1 && j<n2)
+        if(left[i] <= right[j])
+            arr[k++] = left[i++];
+        else
+            arr[k++] = right[j++];
+    
+
+    while(i<n1)
+        arr[k++] = left[i++];
+    while(j<n2)
+        arr[k++] = right[j++];
+}
+void mergeSort(int arr[], int l, int r){
+    if(l<r){
+        int mid = l+(r-l)/2;
+
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+
+        merge(arr,l,mid,r);
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter value of each element: ";
+    for(int i = 0; i<n;i++){
+        cin>>arr[i];
+
+    }
+    mergeSort(arr.data(),0,n-1);
+    cout<<"Sorted Array: ";
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+}
