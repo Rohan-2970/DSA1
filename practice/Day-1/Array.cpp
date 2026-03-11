37ec1737266d9c03cb1b6e58cac39653d22c4a87
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-12 00:05:56 +0530

    largest array

--- practice/Day-1/Array.cpp
+++ practice/Day-1/Array.cpp
@@ -1,19 +1,29 @@
-// #include<iostream>
-// #include<vector>
-// using namespace std;
+// Find the largest element in an array.
 
-// int main(){
-//     int n;
-//     cout<<"Enter the elements: ";
-//     cin>>n;
+#include<iostream>
+#include<vector>
+using namespace std;
 
-//     vector<int>arr(n);
-//     cout<<"Enter the elments: ";
-//     for(int i = 0; i<n; i++){
-//         cin>>arr[i];
-//     }
-//     cout<<"Array: ";
-//     for(int i=0; i<n;i++){
-//         cout<<arr[i]<<" ";
-//     }
-// }
+int main(){
+    int n;
+    cout<<"Enter the elements: ";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter the elments: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Array: ";
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    int largest = arr[0];
+    for(int i = 1; i<n; i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+    cout<<endl;
+    cout<<"Largest elements: "<<largest;
+}
