aa1af856c946f356f725d1e05c44efd6dc5dd66d
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-11 23:19:59 +0530

    Traversal

--- Chatgpt practice/Day-7/String_Traversal.cpp
+++ Chatgpt practice/Day-7/String_Traversal.cpp
@@ -7,3 +7,15 @@
 
 // Traversal order:
 // h → e → l → l → o
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s = "Hello";
+    cout<<"Traversal the string: "<<endl;
+    for(int i=0; i<s.length(); i++){
+        cout<<s[i]<<" ";
+    }
+}
