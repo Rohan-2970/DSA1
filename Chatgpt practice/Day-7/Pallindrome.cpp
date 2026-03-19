49d18f4e7dcb20bf82ca281ea40bddebf5387776
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-19 14:51:30 +0530

    pallindrome

---
+++ Chatgpt practice/Day-7/Pallindrome.cpp
@@ -0,0 +1,27 @@
+// A string that reads the same forward and backward.
+
+// Examples"
+
+// ✅ madam
+// ✅ level
+// ❌ hello
+
+// Logic
+
+// Compare:
+// first ↔ last
+// move inward
+
+// Dry Run Visualization ---> Manually executing 
+// your code step-by-step using a sample input
+// General Pattern:
+// Start → Compare → Move inward → Repeat
+// [l] ----> <---- [r]
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    
+}
