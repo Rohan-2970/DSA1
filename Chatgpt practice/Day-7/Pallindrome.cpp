4d56ac757ad9e663fd831e40eb8df1bc22ea8f63
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-19 16:07:51 +0530

    palindrome

--- Chatgpt practice/Day-7/Pallindrome.cpp
+++ Chatgpt practice/Day-7/Pallindrome.cpp
@@ -21,7 +21,28 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+bool isPallindrome(string s){
+// is this string a boolean means it is true or not
+        int l =0;
+        int r = s.length() - 1;
+        while(l<r){
+            if(s[l] != s[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
 int main(){
-    string s;
-    
+string s;
+cout<<"Enter the string number: ";
+cin>>s;
+
+if(isPallindrome(s))
+    cout<<"palindrome ";
+else
+    cout<<"Not Pallindrome ";
+
 }
