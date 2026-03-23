44e0f0d440c3aed5691effb77e6901fc6b54d1da
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-24 01:28:19 +0530

    anagram

---
+++ practice/strings/Check_Anagram.cpp
@@ -0,0 +1,18 @@
+// ✅ 5. Check Anagram
+// Problem:
+// Check if two strings are anagrams.
+
+// Input:
+// listen
+// silent
+
+// Output:
+// Anagram
+
+// 👉 Focus:
+// Frequency array (freq[26])
+
+#include<iostream>
+#include<string>
+using namespace std;
+
