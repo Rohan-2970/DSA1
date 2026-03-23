c4c700615d1be6bc315c265826ce64ab6ccc7d0a
Rohan-2970 <rohansaxena0905@gmail.com>
2026-03-24 02:11:41 +0530

    Anagram

--- practice/strings/Check_Anagram.cpp
+++ practice/strings/Check_Anagram.cpp
@@ -16,3 +16,35 @@
 #include<string>
 using namespace std;
 
+bool isAnagram(string a, string b){
+    if(a.length() != b.length()) // here the length of the strings are checked
+        return false;
+
+    int freq[26] ={};
+// frequency of the array is given here 
+
+    for(char c : a){
+        freq[c - 'a']++;
+    }
+    // counting the char of the first string 
+
+    for(char c : b){
+        freq[c - 'a']--;
+    }
+
+    for(int i=0; i<26; i++){
+        if(freq[i] != 0)
+        return false;
+    }
+    return true;
+}
+
+int main(){
+    string a, b;
+    cout<<"Enter two string: ";
+    cin>> a>>b;
+    if(isAnagram(a,b))
+        cout<<"Anagram";
+    else
+        cout<<"Not Anagram";
+}
