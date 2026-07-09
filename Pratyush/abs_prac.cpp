05cf2bbfba6107101330c1fc37c25e3f3bcdbf49
Rohan-2970 <rohansaxena0905@gmail.com>
2026-07-09 16:17:46 +0530

    abs

---
+++ Pratyush/abs_prac.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter 2 numbers: ";
+    cin>>a>>b;
+    int c;
+    c= abs(a-b);
+    cout<<"Difference between 2 numbers: "<<c;
+}
